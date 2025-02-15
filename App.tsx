10933bd25fbbd8267317b4802c86cc95918d79fc
willdant <danielwillian532@gmail.com>
2025-02-14 21:03:58 -0300

    Adicionando projeto da aula 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Tranca pontos</Text>
+      <Text style={styles.subtitles}>O app para você se divertir com seus amigos</Text>
+      <Image source={require('./assets/icon.png')} style={{width: 120, height: 120}} />
+      <Button title='Botao 1' color={'lightcoral'}/>
+      <Text style = {[styles.title, styles.player1]}>0</Text>
+      <Button title='Botao 2' onPress={()=> alert('Love you Gege')}/>
+      <Text style = {[styles.title, styles.player2]}>0</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  player1: {
+    color: 'lightcoral',
+  },
+  player2: {
+    color: 'darkblue',
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 50,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  subtitles: {
+    fontSize: 12,
+    color: 'darkblue',
   },
 });
